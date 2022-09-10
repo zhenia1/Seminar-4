af1b2ff07c691d35a604e7741eafb490d97b15b6
zhenya1 <111535920+zhenia1@users.noreply.github.com>
2022-09-11 02:31:30 +0500

    TASK 4.Functions continued

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int[] ar = AskNumbers();
+WriteLine($"{String.Join(",", ar)} -> [{String.Join(",", ar)}]");
+
+int[] AskNumbers()
+{
+    int[] ar = new int[8];
+    for (int i = 0; i < ar.Length; i++)
+    {
+        ar[i] = AskNumber(i + 1);
+    }
+    return ar;
+}
+
+int AskNumber(int i)
+{
+    Write($"Введите число {i}: ");
+    return int.Parse(ReadLine());
+}
